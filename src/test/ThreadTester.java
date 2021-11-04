2652e3ef61de5b34e6583d2e8bda81522a1e87f3
EdgarCampuzanoB <passwordgithub.1226>
2021-11-03 22:22:29 -0600

    correccion en la clase ThreadTester

--- src/test/ThreadTester.java
+++ src/test/ThreadTester.java
@@ -9,13 +9,12 @@ public class ThreadTester {
         PrintMe p2 = new PrintMe();
         PrintMe p3 = new PrintMe();
 
-
         Thread t = new Thread(p1);
-        t.start();
-
         Thread t2 = new Thread(p2);
-        t2.start();
         Thread t3 = new Thread(p3);
+
+        t.start();
+        t2.start();
         t3.start();
 
 
--- src/threads/PrintMe.java
+++ src/threads/PrintMe.java
@@ -4,15 +4,13 @@ public class PrintMe implements Runnable {
 
     @Override
     public void run() {
-        for (short i = 1; i<=10; i++){
-            System.out.println("numero " + i);
-            System.out.println( Thread.currentThread() );
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("numero " + i + " - " + Thread.currentThread().getName());
             }
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println("Esto es un error");
         }
-
-
     }
 }
